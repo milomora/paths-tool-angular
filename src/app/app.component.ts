426f826d8cbc85d3f5534a94487243acc43d51e7
Emilio Mora <emilio.mora@accenture.com>
2024-11-27 08:16:10 -0600

    - Create Angular app   - Angular CLI     - ng new     - ng generate     - ng lint   - Strict mode (TS Config)   - Components basic structure (Template/Styles/Logic)   - Modules (legacy) vs Standalone (new)     - Documentation     - Legacy       - Modules structure       - Declaration/import/export     - New       - Folder structure       - Imports   - Add lint / formatter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainComponent } from './core/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'paths-tool-angular';
